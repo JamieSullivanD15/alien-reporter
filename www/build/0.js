467aa96f7b05d8b4e91955b9b3ed3f2151e961bb
Jamie Sullivan <jamiesullivan523@gmail.com>
2017-11-06 18:25:45 +0000

    Added www Files

---
+++ www/build/0.js
@@ -0,0 +1,131 @@
+webpackJsonp([0],{
+
+/***/ 264:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "SightingsPageModule", function() { return SightingsPageModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(98);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__sightings__ = __webpack_require__(265);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+var SightingsPageModule = (function () {
+    function SightingsPageModule() {
+    }
+    return SightingsPageModule;
+}());
+SightingsPageModule = __decorate([
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["L" /* NgModule */])({
+        declarations: [
+            __WEBPACK_IMPORTED_MODULE_2__sightings__["a" /* SightingsPage */],
+        ],
+        imports: [
+            __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* IonicPageModule */].forChild(__WEBPACK_IMPORTED_MODULE_2__sightings__["a" /* SightingsPage */]),
+        ],
+    })
+], SightingsPageModule);
+
+//# sourceMappingURL=sightings.module.js.map
+
+/***/ }),
+
+/***/ 265:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return SightingsPage; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(98);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_http__ = __webpack_require__(99);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_rxjs_add_operator_map__ = __webpack_require__(194);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_rxjs_add_operator_map___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_3_rxjs_add_operator_map__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__providers_data_data__ = __webpack_require__(195);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
+
+
+/*
+  Main Component of application.
+  Holds all data to be displayed as well as
+  navigation functions and data provider.
+*/
+var SightingsPage = (function () {
+    function SightingsPage(navCtrl, http, dataProvider) {
+        this.navCtrl = navCtrl;
+        this.http = http;
+        this.dataProvider = dataProvider;
+        this.alienSightings = [];
+        this.sighting = {};
+        this.editing = false;
+        this.viewing = false;
+        this.reorderButton = 'Reorder';
+        // Upon startup, assign alienSightings the data returned from dataProvider.
+        this.alienSightings = this.dataProvider.getSightings();
+    }
+    /*
+      When the item is dragged and dropped into the new position, the (ionItemReorder) event is emitted.
+      This event provides the initial index (from) and the new index (to) of the reordered item.
+      For example, if the first item is dragged to the fifth position, the event will emit {from: 0, to: 4}
+    */
+    SightingsPage.prototype.reorderItem = function (indexes) {
+        this.sighting.form = Object(__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["g" /* reorderArray */])(this.sighting.form, indexes);
+    };
+    /*
+      Toggles between sighting list and sighting object details.
+      Passes an index of clicked item and assigns the sighting to be viewed by
+      pulling corresponding object from alienSightings array.
+    */
+    SightingsPage.prototype.toggleView = function (index) {
+        this.sighting = this.alienSightings[index];
+        this.viewing = !this.viewing;
+    };
+    /*
+      Toggles the reorder button on and off.
+      If button is active it allows the the form elements in a sighting object to be reordered
+      by dragging and dropping.
+    */
+    SightingsPage.prototype.toggleReorder = function () {
+        this.editing = !this.editing;
+        this.editing ? this.reorderButton = 'Done' : this.reorderButton = 'Reorder';
+    };
+    // Navigation function that returns to sighting list and resets the reorder button to default value.
+    SightingsPage.prototype.back = function () {
+        this.editing = false;
+        this.editing ? this.reorderButton = 'Done' : this.reorderButton = 'Reorder';
+        this.viewing = !this.viewing;
+    };
+    return SightingsPage;
+}());
+SightingsPage = __decorate([
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
+        selector: 'page-sightings',template:/*ion-inline-start:"C:\Users\Jamie Sullivan\Documents\GitHub\alien-reporter\src\pages\sightings\sightings.html"*/'<ion-header no-padding no-border>\n\n  <ion-navbar no-padding color="primary">\n    <ion-row no-padding justify-content-center>\n      <ion-col col-4 col-md-2 no-padding text-center> <img src="assets/imgs/alien-logo.png" alt=""></ion-col>\n      <ion-col col-8 col-md-10 class="app-header" no-padding align-self-center> Alien Reporter </ion-col>\n    </ion-row>\n  </ion-navbar>\n\n</ion-header>\n\n<!--\n  Main content consists of a grid with a single row and column.\n  The column contains a list of all sightings that were found in JSON endpoint\n  and a div which contains all information associated with a particular sighting.\n  Switching between the views is done with a boolean named \'viewing\'.\n-->\n\n<ion-content no-padding class="content">\n\n  <ion-grid class="main" no-padding>\n    <ion-row no-padding>\n\n      <ion-col>\n        <!-- List of all sightings, show if viewing sighting details is not active -->\n        <ion-list no-padding class="sightings-list" *ngIf="!viewing">\n          <ion-row class="list-header">Sightings</ion-row>\n\n          <ion-item no-padding class="sightings-list-labels">\n            <ion-row justify-content-start>\n              <ion-col col-3 col-xl-2 text-center>Form ID</ion-col>\n              <ion-col col-6 col-xl-2 text-center>Date</ion-col>\n              <ion-col col-3 col-xl-2 text-center>Editor</ion-col>\n            </ion-row>\n          </ion-item>\n\n          <button ion-item no-padding *ngFor="let sighting of alienSightings; let i = index" (click)="toggleView(i)">\n            <ion-row justify-content-start>\n              <ion-col col-3 col-xl-2 text-center>{{sighting.formId}}</ion-col>\n              <ion-col col-6 col-xl-2 text-center>{{sighting.date}}</ion-col>\n              <ion-col col-3 col-xl-2 text-center>{{sighting.lastChangedBy}}</ion-col>\n            </ion-row>\n          </button>\n        </ion-list>\n        <!-- // Sighting list end -->\n\n        <!-- Sighting details, show if viewing sighting details is active -->\n        <div class="sighting" *ngIf="viewing" >\n          <ion-row class="list-header">Form ID: {{sighting.formId}}</ion-row>\n\n          <ion-row>Date: {{sighting.date}}</ion-row>\n          <ion-row>Time: {{sighting.time}}</ion-row>\n          <ion-row>Editor: {{sighting.lastChangedBy}}</ion-row>\n\n          <!-- Form details with reorderable list-->\n          <ion-list class="sighting-form"  [reorder]="editing" (ionItemReorder)="reorderItem($event)" no-padding>\n            <ion-item *ngFor="let item of sighting.form" no-padding>\n              <ion-row no-padding class="sighting-form-id">{{item.id}}</ion-row>\n              <ion-row no-padding>\n                <ion-col col-12 col-lg-2>{{item.caption}}</ion-col>\n                <ion-col col-12 col-lg-2>{{item.type}}</ion-col>\n              </ion-row>\n            </ion-item>\n          </ion-list>\n\n          <ion-row justify-content-start>\n            <button ion-button clear no-padding (click)="back()">\n              <ion-icon name="arrow-back"></ion-icon> Back\n            </button>\n            <button ion-button clear no-padding (click)="toggleReorder()">\n              {{reorderButton}}\n            </button>\n          </ion-row>\n\n        </div>\n        <!-- // Sighting form details end -->\n        </ion-col>\n\n    </ion-row>\n  </ion-grid>\n\n</ion-content>\n'/*ion-inline-end:"C:\Users\Jamie Sullivan\Documents\GitHub\alien-reporter\src\pages\sightings\sightings.html"*/
+    }),
+    __metadata("design:paramtypes", [typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["e" /* NavController */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["e" /* NavController */]) === "function" && _a || Object, typeof (_b = typeof __WEBPACK_IMPORTED_MODULE_2__angular_http__["a" /* Http */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_2__angular_http__["a" /* Http */]) === "function" && _b || Object, typeof (_c = typeof __WEBPACK_IMPORTED_MODULE_4__providers_data_data__["a" /* DataProvider */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_4__providers_data_data__["a" /* DataProvider */]) === "function" && _c || Object])
+], SightingsPage);
+
+var _a, _b, _c;
+//# sourceMappingURL=sightings.js.map
+
+/***/ })
+
+});
+//# sourceMappingURL=0.js.map
---
+++ www/build/main.js
@@ -0,0 +1,233 @@
+webpackJsonp([1],{
+
+/***/ 108:
+/***/ (function(module, exports) {
+
+function webpackEmptyAsyncContext(req) {
+	// Here Promise.resolve().then() is used instead of new Promise() to prevent
+	// uncatched exception popping up in devtools
+	return Promise.resolve().then(function() {
+		throw new Error("Cannot find module '" + req + "'.");
+	});
+}
+webpackEmptyAsyncContext.keys = function() { return []; };
+webpackEmptyAsyncContext.resolve = webpackEmptyAsyncContext;
+module.exports = webpackEmptyAsyncContext;
+webpackEmptyAsyncContext.id = 108;
+
+/***/ }),
+
+/***/ 150:
+/***/ (function(module, exports, __webpack_require__) {
+
+var map = {
+	"../pages/sightings/sightings.module": [
+		264,
+		0
+	]
+};
+function webpackAsyncContext(req) {
+	var ids = map[req];
+	if(!ids)
+		return Promise.reject(new Error("Cannot find module '" + req + "'."));
+	return __webpack_require__.e(ids[1]).then(function() {
+		return __webpack_require__(ids[0]);
+	});
+};
+webpackAsyncContext.keys = function webpackAsyncContextKeys() {
+	return Object.keys(map);
+};
+webpackAsyncContext.id = 150;
+module.exports = webpackAsyncContext;
+
+/***/ }),
+
+/***/ 195:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return DataProvider; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_http__ = __webpack_require__(99);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_rxjs_add_operator_map__ = __webpack_require__(194);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_rxjs_add_operator_map___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_rxjs_add_operator_map__);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
+var URL = 'http://www.mocky.io/v2/59f7760a2f0000ab1d55864e';
+var DataProvider = (function () {
+    function DataProvider(http) {
+        this.http = http;
+        this.alienSightings = [];
+        this.sighting = {};
+        this.getData();
+    }
+    DataProvider.prototype.setSightings = function (alienSightings) {
+        this.alienSightings = alienSightings;
+    };
+    DataProvider.prototype.getSightings = function () {
+        return this.alienSightings;
+    };
+    DataProvider.prototype.getData = function () {
+        var _this = this;
+        return this.http.get(URL)
+            .subscribe(function (data) {
+            _this.alienSightings.push(data.json()),
+                _this.formatDateAndTime();
+        }, function (error) {
+            console.log(error);
+        });
+    };
+    DataProvider.prototype.formatDateAndTime = function () {
+        var splitDate;
+        for (var i = 0; i < this.alienSightings.length; i++) {
+            this.alienSightings[i].time = this.alienSightings[i].lastChangedDate.split('T')[1].split('+')[0];
+            this.alienSightings[i].date = this.alienSightings[i].lastChangedDate.split('T')[0];
+            splitDate = this.alienSightings[i].date.split('-');
+            this.alienSightings[i].date = splitDate[2] + '/' + splitDate[1] + '/' + splitDate[0];
+        }
+    };
+    return DataProvider;
+}());
+DataProvider = __decorate([
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["B" /* Injectable */])(),
+    __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1__angular_http__["a" /* Http */]])
+], DataProvider);
+
+//# sourceMappingURL=data.js.map
+
+/***/ }),
+
+/***/ 196:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser_dynamic__ = __webpack_require__(197);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__app_module__ = __webpack_require__(215);
+
+
+Object(__WEBPACK_IMPORTED_MODULE_0__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_1__app_module__["a" /* AppModule */]);
+//# sourceMappingURL=main.js.map
+
+/***/ }),
+
+/***/ 215:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__(25);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_ionic_angular__ = __webpack_require__(98);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__ionic_native_splash_screen__ = __webpack_require__(190);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__ionic_native_status_bar__ = __webpack_require__(193);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__angular_http__ = __webpack_require__(99);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__app_component__ = __webpack_require__(263);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__providers_data_data__ = __webpack_require__(195);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+
+
+
+
+
+var AppModule = (function () {
+    function AppModule() {
+    }
+    return AppModule;
+}());
+AppModule = __decorate([
+    Object(__WEBPACK_IMPORTED_MODULE_1__angular_core__["L" /* NgModule */])({
+        declarations: [
+            __WEBPACK_IMPORTED_MODULE_6__app_component__["a" /* MyApp */]
+        ],
+        imports: [
+            __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
+            __WEBPACK_IMPORTED_MODULE_2_ionic_angular__["c" /* IonicModule */].forRoot(__WEBPACK_IMPORTED_MODULE_6__app_component__["a" /* MyApp */], {}, {
+                links: [
+                    { loadChildren: '../pages/sightings/sightings.module#SightingsPageModule', name: 'SightingsPage', segment: 'sightings', priority: 'low', defaultHistory: [] }
+                ]
+            }),
+            __WEBPACK_IMPORTED_MODULE_5__angular_http__["b" /* HttpModule */]
+        ],
+        bootstrap: [__WEBPACK_IMPORTED_MODULE_2_ionic_angular__["a" /* IonicApp */]],
+        entryComponents: [
+            __WEBPACK_IMPORTED_MODULE_6__app_component__["a" /* MyApp */]
+        ],
+        providers: [
+            __WEBPACK_IMPORTED_MODULE_4__ionic_native_status_bar__["a" /* StatusBar */],
+            __WEBPACK_IMPORTED_MODULE_3__ionic_native_splash_screen__["a" /* SplashScreen */],
+            { provide: __WEBPACK_IMPORTED_MODULE_1__angular_core__["v" /* ErrorHandler */], useClass: __WEBPACK_IMPORTED_MODULE_2_ionic_angular__["b" /* IonicErrorHandler */] },
+            __WEBPACK_IMPORTED_MODULE_7__providers_data_data__["a" /* DataProvider */],
+            __WEBPACK_IMPORTED_MODULE_5__angular_http__["b" /* HttpModule */]
+        ]
+    })
+], AppModule);
+
+//# sourceMappingURL=app.module.js.map
+
+/***/ }),
+
+/***/ 263:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return MyApp; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(98);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__ionic_native_status_bar__ = __webpack_require__(193);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__ionic_native_splash_screen__ = __webpack_require__(190);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
+
+var MyApp = (function () {
+    function MyApp(platform, statusBar, splashScreen) {
+        this.rootPage = 'SightingsPage';
+        platform.ready().then(function () {
+            // Okay, so the platform is ready and our plugins are available.
+            // Here you can do any higher level native things you might need.
+            statusBar.styleDefault();
+            splashScreen.hide();
+        });
+    }
+    return MyApp;
+}());
+MyApp = __decorate([
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({template:/*ion-inline-start:"C:\Users\Jamie Sullivan\Documents\GitHub\alien-reporter\src\app\app.html"*/'<ion-nav [root]="rootPage"></ion-nav>\n'/*ion-inline-end:"C:\Users\Jamie Sullivan\Documents\GitHub\alien-reporter\src\app\app.html"*/
+    }),
+    __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["f" /* Platform */], __WEBPACK_IMPORTED_MODULE_2__ionic_native_status_bar__["a" /* StatusBar */], __WEBPACK_IMPORTED_MODULE_3__ionic_native_splash_screen__["a" /* SplashScreen */]])
+], MyApp);
+
+//# sourceMappingURL=app.component.js.map
+
+/***/ })
+
+},[196]);
+//# sourceMappingURL=main.js.map
